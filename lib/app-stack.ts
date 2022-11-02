64fa038815ce8c4872cdc71d533832a7ad3fe733
EC2 Default User <ec2-user@ip-172-31-17-141.ap-northeast-1.compute.internal>
2022-11-02 07:09:31 +0000

    update circleci config file

--- lib/app-stack.ts
+++ lib/app-stack.ts
@@ -1,10 +1,10 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+//import * as cdk from 'aws-cdk-lib';
+//import { Construct } from 'constructs';
+//// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
-export class AppStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
-    super(scope, id, props);
+//export class AppStack extends cdk.Stack {
+//  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+//    super(scope, id, props);
 
     // The code that defines your stack goes here
 
@@ -12,5 +12,23 @@ export class AppStack extends cdk.Stack {
     // const queue = new sqs.Queue(this, 'AppQueue', {
     //   visibilityTimeout: cdk.Duration.seconds(300)
     // });
-  }
+//  }
+//}
+
+
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+
+export class AppStack extends cdk.Stack {
+	constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+		super(scope, id, props);
+
+		// 아래 내용을 추가합니다
+		const fn = new lambda.Function(this, 'LambdaFunction', {
+			runtime: lambda.Runtime.NODEJS_14_X,
+			handler: 'index.handler',
+			code: lambda.Code.fromAsset('lambda/')
+		});
+	}
 }
